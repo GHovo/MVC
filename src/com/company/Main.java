658292cd3efef85721644d82d48b6d57bcfeec49
Araksya Hambaryan <ahambaryan@beachbody.com>
2020-08-16 14:28:41 +0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import controller.StudentController;
+import model.Student;
+import view.StudentView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student model  = retriveStudentFromDatabase();
+
+        //Create a view : to write student details on console
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model, view);
+
+        controller.updateView();
+
+        //update model data
+        controller.setStudentName("John");
+
+        controller.updateView();
+    }
+
+    private static Student retriveStudentFromDatabase(){
+        Student student = new Student();
+        student.setName("Robert");
+        student.setRollNo("10");
+        return student;
+    }
+}
